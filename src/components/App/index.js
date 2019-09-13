e699fe10991e2313c6cbe64e7ea4e609c5028a14
Benjamin Fransson <benjamin@cafcon.se>
2019-09-13 11:06:01 +0200

    Scaffolded swipe component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
+import SwipeComponent from '../SwipeComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <h1>Hello World</h1>
+        <SwipeComponent/>
     </div>
   );
 }
