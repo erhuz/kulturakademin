53b9f78c574c03912bba132f2f358ef5ccef071d
Amin <hamadagood111@gmail.com>
2019-09-13 20:09:44 +0200

    Starting with navigation

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+import logo from '../../assets/images/logo.png'
+import menuIcon from '../../assets/images/menu.png'
+
+const Header = () => (
+    <header>
+        <div className="navbar">
+            <a href="#"><img className="logo" src={logo} alt="logo"/></a>
+            <a href="#"><img className="menu" src={menuIcon} alt="menu"/></a>
+        </div>
+    </header>
+)
+
+export default Header
