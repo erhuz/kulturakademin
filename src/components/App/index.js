caa93bb2931412196a75e0a51025287a98b4274a
Samuel <sumlebulle@hotmail.com>
2019-09-16 11:37:54 +0200

    Created categories button component, categories array and mapped in app.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import SwipeContainer from '../SwipeContainer';
 import Header from '../Header';
 import Footer from '../Footer';
+import CategoriesButton from '../CategoriesButton'
+import categoriesData from '../CategoriesButton/categoriesData'
 import './App.css';
 
 function App() {
+
+  const categoriesComponents = categoriesData.map(item => <CategoriesButton key={item.id} name={item.name} category={item.category} color={item.color}/>)
+
   return (
     <div className="App">
         <Header />
+        {categoriesComponents}
         <SwipeContainer/>
         <h1 > Hello World </h1>
         <Footer />
---
+++ src/components/CategoriesButton/categoriesData.js
@@ -0,0 +1,40 @@
+const categoriesData = [
+    {
+        id: 1,
+        name: "Dans",
+        category: "",
+        color: "250, 108, 103"
+    },
+    {
+        id: 2,
+        name: "Teater",
+        category: "",
+        color: "205, 93, 141"
+    },
+    {
+        id: 3,
+        name: "Konst",
+        category: "",
+        color: "138, 94, 151"
+    },
+    {
+        id: 4,
+        name: "Kategori",
+        category: "",
+        color: "75, 88, 129"
+    },
+    {
+        id: 5,
+        name: "Kategori",
+        category: "",
+        color: "47, 72, 88"
+    },
+    {
+        id: 6,
+        name: "Kategori",
+        category: "",
+        color: "0, 115, 95"
+    },
+]
+
+export default categoriesData
