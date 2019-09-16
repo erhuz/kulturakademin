9d960c6a9a64f6b0fd937113bd9c37995cbee89a
Samuel <sumlebulle@hotmail.com>
2019-09-16 14:26:38 +0200

    Move mapping of categories to a new component. And changed category to route in categoriesData

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import SwipeContainer from '../SwipeContainer';
 import Header from '../Header';
 import Footer from '../Footer';
-import CategoriesButton from '../CategoriesButton'
-import categoriesData from '../CategoriesButton/categoriesData'
+import CategoriesComponent from '../CategoriesComponent'
 import './App.css';
 
 function App() {
 
-  const categoriesComponents = categoriesData.map(item => <CategoriesButton key={item.id} name={item.name} category={item.category} color={item.color}/>)
+
 
   return (
     <div className="App">
         <Header />
-        {categoriesComponents}
+        <CategoriesComponent />
         <SwipeContainer/>
         <h1 > Hello World </h1>
         <Footer />
---
+++ src/components/CategoriesComponent/index.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import CategoriesButton from '../CategoriesButton'
+import categoriesData from './categoriesData'
+
+
+class CategoriesComponent extends Component {
+  render() {
+
+    const categoriesComponents = categoriesData.map(item => <CategoriesButton key={item.id} name={item.name} category={item.category} color={item.color}/>)
+
+    return(
+      <div>
+        {categoriesComponents}
+      </div>
+    )
+  }
+}
+
+export default CategoriesComponent
