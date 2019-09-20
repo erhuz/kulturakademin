3cbbd1e7717f5efdaea3f50c15e906cc49a77a20
Benjamin Fransson <benjamin@cafcon.se>
2019-09-20 12:04:42 +0200

    Added Podcast, Webcast & Individual Category pages

---
+++ src/pages/Webcast/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import DefaultLayout from '../../layouts/Default'
+import './Webcast.css'
+
+class Webcast extends Component {
+  render() {
+    return (
+      <DefaultLayout>
+
+      </DefaultLayout>
+    )
+  }
+}
+
+export default Webcast
+
