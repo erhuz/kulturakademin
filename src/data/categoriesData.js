1d12c7cef64971e73b8f14c6bd50f0ddc1967ad9
Samuel <sumlebulle@hotmail.com>
2019-09-20 11:50:47 +0200

    Changed categoriesButton. Small fixes.

--- src/data/categoriesData.js
+++ src/data/categoriesData.js
@@ -3,37 +3,31 @@ const categoriesData = [
         id: 1,
         name: "Dans",
         route: "",
-        color: "250, 108, 103"
     },
     {
         id: 2,
         name: "Teater",
         route: "",
-        color: "205, 93, 141"
     },
     {
         id: 3,
-        name: "Konst",
+        name: "Film",
         route: "",
-        color: "138, 94, 151"
     },
     {
         id: 4,
-        name: "Kategori",
+        name: "Musik",
         route: "",
-        color: "75, 88, 129"
     },
     {
         id: 5,
-        name: "Kategori",
+        name: "Kulturarbete",
         route: "",
-        color: "47, 72, 88"
     },
     {
         id: 6,
-        name: "Kategori",
+        name: "Digital teknik",
         route: "",
-        color: "0, 115, 95"
     },
 ]
 
