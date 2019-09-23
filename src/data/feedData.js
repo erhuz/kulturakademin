2653be5f6fe08a8b4cfa00f3aa52345b87b0e182
Samuel <sumlebulle@hotmail.com>
2019-09-23 14:52:12 +0200

    Added dummy data to feedData

--- src/data/feedData.js
+++ src/data/feedData.js
@@ -1,33 +1,276 @@
 const feedData = [
   {
-    id: 1,
-    type: "",
-    description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
-    header: "Lorem ipsum",
+    Dans: [
+      {
+        id: 1,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem ipsum",
+      },
+      {
+        id: 2,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 2",
+      },
+      {
+        id: 3,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 3",
+      },
+      {
+        id: 4,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 4",
+      },
+      {
+        id: 5,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 5",
+      },
+    ]
   },
   {
-    id: 2,
-    type: "",
-    description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
-    header: "Lorem 2",
+    Teater: [
+      {
+        id: 1,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem ipsum",
+      },
+      {
+        id: 2,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 2",
+      },
+      {
+        id: 3,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 3",
+      },
+      {
+        id: 4,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 4",
+      },
+      {
+        id: 5,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 5",
+      },
+    ]
   },
   {
-    id: 3,
-    type: "",
-    description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
-    header: "Lorem 3",
+    Film: [
+      {
+        id: 1,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem ipsum",
+      },
+      {
+        id: 2,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 2",
+      },
+      {
+        id: 3,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 3",
+      },
+      {
+        id: 4,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 4",
+      },
+      {
+        id: 5,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 5",
+      },
+    ]
   },
   {
-    id: 4,
-    type: "",
-    description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
-    header: "Lorem 4",
+    Musik: [
+      {
+        id: 1,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem ipsum",
+      },
+      {
+        id: 2,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 2",
+      },
+      {
+        id: 3,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 3",
+      },
+      {
+        id: 4,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 4",
+      },
+      {
+        id: 5,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 5",
+      },
+    ]
   },
   {
-    id: 5,
-    type: "",
-    description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
-    header: "Lorem 5",
+    Kulturarbete: [
+      {
+        id: 1,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem ipsum",
+      },
+      {
+        id: 2,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 2",
+      },
+      {
+        id: 3,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 3",
+      },
+      {
+        id: 4,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 4",
+      },
+      {
+        id: 5,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 5",
+      },
+    ]
+  },
+  {
+    DigitalTeknik: [
+      {
+        id: 1,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem ipsum",
+      },
+      {
+        id: 2,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 2",
+      },
+      {
+        id: 3,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 3",
+      },
+      {
+        id: 4,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 4",
+      },
+      {
+        id: 5,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 5",
+      },
+    ]
+  },
+  {
+    Sparat: [
+      {
+        id: 1,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem ipsum",
+      },
+      {
+        id: 2,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 2",
+      },
+      {
+        id: 3,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 3",
+      },
+      {
+        id: 4,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 4",
+      },
+      {
+        id: 5,
+        type: "",
+        thumbnail: "",
+        description: "Nullam cursus lacinia erat. Vestibulum fringilla pede sit amet augue. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.",
+        header: "Lorem 5",
+      },
+    ]
   },
 ]
 
