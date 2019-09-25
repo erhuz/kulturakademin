5b0da9f0e62b294c62e8ae2c76f11be0a3915252
Amin <hamadagood111@gmail.com>
2019-09-25 23:19:33 +0200

    Added new hamburger menu icon and fixed css

--- src/data/navlinks.js
+++ src/data/navlinks.js
@@ -1,26 +1,14 @@
 const navlinks = [
     {
-      text: 'Logga in',
-      path: '/'
-    },
-    {
       text: 'Hjälp',
       path: '/'
     },
     {
-      text: 'Sekretess',
-      path: '/'
-    },
-    {
-      text: 'Tillgänglighet',
-      path: '/'
-    },
-    {
-      text: 'Sparat',
-      path: '/'
-    },
-    {
       text: 'Välj språk',
+      path: '/'
+    },
+    {
+      text: 'Kontakta oss',
       path: '/'
     }
 ];
