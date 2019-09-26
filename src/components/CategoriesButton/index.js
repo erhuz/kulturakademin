c21b3373294e3fd045c1b5ca6e49e704ac775805
Samuel <sumlebulle@hotmail.com>
2019-09-26 10:27:07 +0200

    Changed class to function. And fixed indent

--- src/components/CategoriesButton/index.js
+++ src/components/CategoriesButton/index.js
@@ -1,20 +1,18 @@
-import React, {Component} from 'react'
+import React from 'react'
 import arrow from '../../assets/images/arrow.png'
 import './CategoriesButton.css'
 import { Link } from 'react-router-dom'
 
-class CategoriesButton extends Component {
-    render() {
-        return(
-          <Link to={{pathname:"/category/"+this.props.category}}>
-            <button className="categories-button">
-                <span className="categories-text">{this.props.category}</span>
-                <img className="categories-arrow" src={arrow} alt=""/>
-            </button>
-          </Link>
 
-        )
-    }
+const CategoriesButton = (props) =>  {
+  return(
+    <Link to={{pathname:"/category/"+props.category}}>
+      <button className="categories-button">
+        <span className="categories-text">{props.category}</span>
+        <img className="categories-arrow" src={arrow} alt=""/>
+      </button>
+    </Link>
+  )
 }
 
 export default CategoriesButton
