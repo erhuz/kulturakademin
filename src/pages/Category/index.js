f7c0f6b9798f2de74f51178d412a5dec7d5ae870
Amin <hamadagood111@gmail.com>
2019-09-26 15:09:23 +0200

    Added back-button to category page

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import DefaultLayout from '../../layouts/Default'
 import FeedContainer from '../../components/FeedContainer'
 import './Category.css'
+import BackButton from '../../components/BackButton'
 
 class Category extends Component {
   render() {
     return (
       <DefaultLayout>
+        <BackButton />
         <h2 className="category-header">{this.props.match.params.name}</h2>
         <FeedContainer category={this.props.match.params.name}/>
       </DefaultLayout>
