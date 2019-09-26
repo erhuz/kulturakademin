d810e047237d39ea6b1423b276d1fa45f7eafdb9
Amin <hamadagood111@gmail.com>
2019-09-26 19:19:26 +0200

    Fixed a bug in hamburger menu botton

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './Header.css';
+import menuIcon from '../../assets/images/hamburger-menu.svg';
+import facebookIcon from '../../assets/images/Vector2.svg';
+import instagramIcon from '../../assets/images/Vector3.svg';
+import twitterIcon from '../../assets/images/Vector1.svg';
+import linkedinIcon from '../../assets/images/Vector.svg';
+import closeButton from '../../assets/images/x.svg';
+import navlinks from '../../data/navlinks';
+import linkTo from '../../assets/images/to.svg';
 import classnames from "classnames";
-import MenuItems from '../MenuItems';
-import BackButton from '../BackButton';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from '../../assets/images/logo.svg';
-import menuIcon from '../../assets/images/hamburger-menu.svg';
 
 class Header extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.handleClick = this.handleClick.bind(this);
 
@@ -18,14 +23,13 @@ class Header extends React.Component {
       prevScrollpos: window.pageYOffset,
       visible: true,
       showBackButton: false,
-      menuIsOn: false
+      menuIsOn: false,
     };
   };
 
-  handleClick (e) {
-    e.preventDefault();
+  handleClick () {
       this.setState({
-        menuIsOn: !this.state.menuIsOn
+        menuIsOn: !this.state.menuIsOn,
     });
   }
 
@@ -66,8 +70,35 @@ class Header extends React.Component {
         </Link>
 
           {this.state.menuIsOn && (
-            <MenuItems />
+
+            <div className="menu">
+              <div className="close-button">
+                <a onClick={this.handleClick}  > <img src={closeButton} alt="icon"/></a>
+              </div>
+              <div className="menu-tittle">
+                <h1>Meny</h1>
+                <hr className="line"></hr>
+              </div>
+              <div className="menu-items">
+                {navlinks.map(link =>
+                  <Link to={link.path} key={link.id}>{link.text}</Link>
+                )}
+              </div>
+              <div className="link-to">
+                <a href="https://www.kulturakademin.com">Kulturakademin</a>
+                <img src={linkTo} alt="icon"/>
+              </div>
+              <hr className="line-2"></hr>
+              <div className="social-media">
+                  <p>Följ oss</p>
+                  <Link to=""><img src={instagramIcon} alt="icon"/></Link>
+                  <Link to=""><img src={facebookIcon} alt="icon"/></Link>
+                  <Link to=""><img src={twitterIcon} alt="icon"/></Link>
+                  <Link to=""><img src={linkedinIcon} alt="icon"/></Link>
+              </div>
+          </div>
           )}
+
       </nav>
     )
   }
