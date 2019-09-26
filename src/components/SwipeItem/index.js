0315a72ef05a119e76dda2cbb9bd5473f6664b9f
Benjamin Fransson <benjamin@cafcon.se>
2019-09-26 17:37:18 +0200

    Added swipe-item content from youtube api res

--- src/components/SwipeItem/index.js
+++ src/components/SwipeItem/index.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import TimeAgo from 'javascript-time-ago';
+import sv from 'javascript-time-ago/locale/sv'
 import './SwipeItem.css';
 
 class SwipeItem extends Component {
@@ -11,31 +14,81 @@ class SwipeItem extends Component {
   }
 
   render() {
-    const {index, backgroundUrl} = this.props;
+    const {item} = this.props;
 
-
-    const swipeItem = {
-      backgroundColor: 'green',
-      borderRadius: '8px',
-      flex: '0 0 19.7%',
-      textAlign: 'center',
+    const swipeItemContainer = {
       margin: '0 10.87px',
+      // flex: '0 0 19.7%',
       width: '121px',
       minWidth: '121px',
       maxWidth: '121px',
+
+    }
+
+    const swipeItem = {
+      borderRadius: '8px',
+      textAlign: 'center',
       height: '121px',
       minHeight: '121px',
       maxHeight: '121px',
-      background: 'linear-gradient(225deg, rgba(0,0,0,0) 25%, rgba(255,255,255,0.35) 100%), url(' + backgroundUrl + ')',
+      background: 'linear-gradient(225deg, rgba(0,0,0,0) 25%, rgba(255,255,255,0.35) 100%), url(' + item.snippet.thumbnails.medium.url + ')',
       backgroundSize: 'cover',
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center center',
       userSelect : 'none'
     }
 
+    const h4 = {
+      color: '#fff',
+      fontSize: 14,
+      fontWeight: 'normal',
+      marginTop: '8px',
+      wordWrap: 'break-word',
+    }
+
+    const p = {
+      color: '#fff',
+      opacity: '0.6',
+      fontWeight: 'normal',
+      fontSize: 12,
+      marginTop: '4px'
+    }
+
+    const howLongAgoStyles = {
+      color: '#ccc',
+      fontWeight: 'normal',
+      fontSize: 12,
+    }
+
+    let itemDescription = '';
+
+    if(item.snippet.description.length >= 50){
+      itemDescription = item.snippet.description.substring(0,50) + '...';
+    }else{
+      itemDescription = item.snippet.description
+    }
+
+    let itemTitle = '';
+
+    if(item.snippet.title.length >= 20){
+      itemTitle = item.snippet.title.substring(0,20) + '...';
+    }else{
+      itemTitle = item.snippet.title
+    }
+
+    TimeAgo.addLocale(sv)
+    const timeAgo = new TimeAgo('sv-SE')
+    const howLongAgo = timeAgo.format(new Date(item.snippet.publishedAt)).replace('för ', '');
 
     return (
-        <div style={swipeItem}></div>
+        <div style={swipeItemContainer}>
+          <Link to={'/'}>
+            <div style={swipeItem}></div>
+            <h4 style={h4}>{itemTitle}</h4>
+            <p style={p}>{itemDescription}</p>
+            <p style={howLongAgoStyles}>{howLongAgo}</p>
+          </Link>
+        </div>
     );
   }
 }
