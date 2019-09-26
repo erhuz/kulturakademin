80ca6a4f9bf772dca6628e7ea9ad53263c31eb07
Samuel <sumlebulle@hotmail.com>
2019-09-26 10:30:12 +0200

    Changed class to function

--- src/components/CategoriesButton/index.js
+++ src/components/CategoriesButton/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import arrow from '../../assets/images/arrow.png'
-import './CategoriesButton.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import arrow from '../../assets/images/arrow.png';
+import './CategoriesButton.css';
+import { Link } from 'react-router-dom';
 
 
 const CategoriesButton = (props) =>  {
@@ -12,7 +12,7 @@ const CategoriesButton = (props) =>  {
         <img className="categories-arrow" src={arrow} alt=""/>
       </button>
     </Link>
-  )
-}
+  );
+};
 
-export default CategoriesButton
+export default CategoriesButton;
