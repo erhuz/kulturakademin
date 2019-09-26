0a761f3cad7e8f1d3f587eb56b1fb9cfa2cf6934
Amin <hamadagood111@gmail.com>
2019-09-26 19:58:07 +0200

    Removed pod or video in search component

--- src/components/SearchComponent/index.js
+++ src/components/SearchComponent/index.js
@@ -5,21 +5,25 @@ class SearchComponent extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = {
+      searchResultOn: false
+     };
   }
 
   render() {
     return (
       <div>
         <div className="search-bar">
-          <input className="search-input" type="text" placeholder="Sök"/>
+          <input className="search-input" placeholder="Sök"/>
           <Link className="search-cancel">avbryt</Link>
         </div>
-        <div className="video-or-podd">
-          <Link><p className="video">Video</p></Link>
-          <div className="vertical-line"></div>
-          <Link> <p className="pod">Pod</p></Link>
-        </div>
+          {this.state.searchResultOn && (
+            <div className="video-or-podd">
+              <Link><p className="video">Video</p></Link>
+              <div className="vertical-line"></div>
+              <Link> <p className="pod">Pod</p></Link>
+            </div>
+            )}
       </div>
     );
   }
